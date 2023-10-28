2957405d49960b94b9bd700a029fae8ede93d381
Tuncer Özgür <tuncer_ozgur@icloud.com>
2023-10-28 20:39:08 +0200

    v1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,21 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        //Versiyon 1 icin kodlar.
+
+        int x=1;
+        int y=2;
+
+
+        System.out.println(x+y);
+        System.out.println("Versiyon 1 tamamlandi.");
+
+
+
+
+
+
+
+    }
+
+}
