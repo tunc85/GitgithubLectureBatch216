75cb7dc4ef6b131d02bdb29c51f4144f9e10e0e9
Tuncer Özgür <tuncer_ozgur@icloud.com>
2023-10-28 22:28:32 +0200

    v5

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Class01 {
     public static void main(String[] args) {
 
@@ -19,6 +21,24 @@ public class Class01 {
         //Versiyon 3 icin kodlar.
 
         System.out.println("Versiyon 3 kodlari.");
+
+        //Versiyon 4 icin kodlar
+        Scanner newScanner = new Scanner(System.in);
+        System.out.println("Lütfen Adınızı Giriniz.");
+        String adKismi= newScanner.nextLine();
+
+        System.out.println("Adınız :"+ adKismi);
+        System.out.println("Lütfen Soyadınızı Giriniz. ");
+        String soyAd= newScanner.next();
+        System.out.println("Soyadınız :"+ soyAd);
+        System.out.println(adKismi + " " + soyAd);
+        System.out.println("Başarılı :) Yönlendiriliyorsunuz.....");
+        System.out.println( "Eğer otomatik olarak yönlendirme olmadıysa tıklayınız.");
+
+        String hedefurlSite = "https://lms.techproeducation.com/";
+        System.out.println(hedefurlSite);
+
+
 
 
 
